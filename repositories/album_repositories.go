979f2ffb8c17ef38d7c782ef8217099860c5649e
naufal-abdillah <abdillahnfl@gmail.com>
2021-12-15 15:00:25 +0700

    Deleted useless operations
    deleted prints used for debugging

--- repositories/album_repositories.go
+++ repositories/album_repositories.go
@@ -3,7 +3,6 @@ package repositories
 import (
 	"example/web-service-gin/config"
 	"example/web-service-gin/models"
-	"fmt"
 	"net/http"
 )
 
@@ -12,6 +11,5 @@ type Repo struct {
 
 func (R Repo) RepoGetAlbum() (int, []models.Album) {
 	var Albums = config.SqlQuery()
-	fmt.Println("---------Print Repo----------")
 	return http.StatusOK, Albums
 }
--- services/student_services.go
+++ services/student_services.go
@@ -4,14 +4,12 @@ import (
 	"example/web-service-gin/interfaces"
 	"example/web-service-gin/models"
 	"example/web-service-gin/repositories"
-	"fmt"
 )
 
 type Service struct {
 }
 
 func (S Service) ServicesGetAlbums() (int, []models.Album) {
-	fmt.Println("---------Sebelum return----------")
 	var IRepo interfaces.IAlbumRepo
 	IRepo = repositories.Repo{}
 	// return repositories.RepoGetAlbum()
