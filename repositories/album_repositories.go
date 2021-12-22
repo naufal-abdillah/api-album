e02f2b494efe33535365790ec770e15dd920f3e1
naufal-abdillah <abdillahnfl@gmail.com>
2021-12-22 19:53:54 +0700

    Using sqlx

--- repositories/album_repositories.go
+++ repositories/album_repositories.go
@@ -1,19 +1,24 @@
 package repositories
 
 import (
-	"example/web-service-gin/config"
 	"example/web-service-gin/models"
 	"fmt"
 	"net/http"
+
+	"github.com/jmoiron/sqlx"
 )
 
 func SqlQuery() []models.Album {
-	db, err := config.Connect()
+	// db, err := config.Connect()
+	db, err := sqlx.Connect("mysql", "root:@tcp(127.0.0.1:3306)/db_belajar_golang")
 	if err != nil {
 		fmt.Println(err.Error())
 		// return
 	}
 	defer db.Close()
+
+	Albums := []models.Album{}
+	db.Select(&Albums, "select * from tb_album")
 
 	rows, err := db.Query("select * from tb_album")
 	if err != nil {
@@ -41,9 +46,11 @@ func SqlQuery() []models.Album {
 		// return
 	}
 	output := make([]models.Album, len(result))
-	for _, each := range result {
-		output = append(output, each)
-	}
+	// for _, each := range result {
+	// 	output = append(output, each)
+	// }
+	output = append(output, result...)
+
 	return output
 }
 
